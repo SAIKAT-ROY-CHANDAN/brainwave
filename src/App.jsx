ed674b96067f6d1da81b4a95bd6828fee88fc79b
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-06-21 12:50:50 +0600

    collbartion section is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
-// import Collaboration from "./components/Collaboration";
+import Collaboration from "./components/Collaboration";
 // import Footer from "./components/Footer";
 // import Pricing from "./components/Pricing";
 // import Roadmap from "./components/Roadmap";
@@ -15,8 +15,8 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
-        {/* <Collaboration />
-        <Services />
+        <Collaboration />
+        {/* <Services />
         <Pricing />
         <Roadmap />
         <Footer /> */}
