5b601d38d6acf1b43832067bb1278f0d50ae9dbf
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-06-23 15:13:12 +0600

    roadmap section is complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing.jsx";
-// import Footer from "./components/Footer";
-// import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
+import Roadmap from "./components/Roadmap";
 
 const App = () => {
   return (
@@ -18,8 +18,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
-        {/* <Roadmap />
-        <Footer /> */}
+        <Roadmap />
+        <Footer />
       </div>
 
       <ButtonGradient />
